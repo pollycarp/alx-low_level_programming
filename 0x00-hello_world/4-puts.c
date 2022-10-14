62e463947296ffd476e06b8842bbbc60439451e3
pollycarp <markpollycarp@gmail.com>
2022-10-14 19:47:14 +0300

    Corrections for task 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  *  * main -Entry point
  *   *
- *    * Return 0 always
+ *    *Return: Always 0 (Success) 
  *
  *
  */
 int main(void)
 {
-puts("\"Programming is likebuilding a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
